f57cf25e5dab9ff5f8132dff8a24e546269ce4a4
gustifa <fauzangustifa@gmail.com>
2025-11-26 15:32:48 +0700

    Update Sidebar

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,6 @@
 <x-app-layout>
     <div class="page-content">
+        @role('admin')
 				<div class="row row-cols-1 row-cols-md-2 row-cols-xl-2 row-cols-xxl-4">
                    <div class="col">
 					 <div class="card radius-10 bg-gradient-cosmic">
@@ -568,4 +569,5 @@
 						</div>
 					 </div><!--end row-->
 			</div>
+            @endrole
 </x-app-layout>
