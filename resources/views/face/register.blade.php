64e742c7831c4e2b97976a2ade1e819b52bf7a39
gustifa <fauzangustifa@gmail.com>
2025-11-26 17:23:19 +0700

    Add Fitur Scan Wajah

--- resources/views/face/register.blade.php
+++ resources/views/face/register.blade.php
@@ -1,4 +1,39 @@
-<x-app-layout>
+<!doctype html>
+<html lang="en">
+
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!--favicon-->
+	<link rel="icon" href="{{ asset('backend/assets/images/favicon-32x32.png')}}" type="image/png"/>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+	<!--plugins-->
+	<link href="{{ asset('backend/assets/plugins/vectormap/jquery-jvectormap-2.0.2.css')}}" rel="stylesheet"/>
+	<link href="{{ asset('backend/assets/plugins/simplebar/css/simplebar.css')}}" rel="stylesheet" />
+	<link href="{{ asset('backend/assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css')}}" rel="stylesheet" />
+	<link href="{{ asset('backend/assets/plugins/metismenu/css/metisMenu.min.css')}}" rel="stylesheet"/>
+	<!-- loader-->
+	<link href="{{ asset('backend/assets/css/pace.min.css')}}" rel="stylesheet"/>
+	<script src="{{ asset('backend/assets/js/pace.min.js')}}"></script>
+	<!-- Bootstrap CSS -->
+	<link href="{{ asset('backend/assets/css/bootstrap.min.css')}}" rel="stylesheet">
+	<link href="{{ asset('backend/assets/css/bootstrap-extended.css')}}" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+	<link href="{{ asset('backend/assets/css/app.css')}}" rel="stylesheet">
+	<link href="{{ asset('backend/assets/css/icons.css')}}" rel="stylesheet">
+	<!-- Theme Style CSS -->
+	<link rel="stylesheet" href="{{ asset('backend/assets/css/dark-theme.css')}}"/>
+	<link rel="stylesheet" href="{{ asset('backend/assets/css/semi-dark.css')}}"/>
+	<link rel="stylesheet" href="{{ asset('backend/assets/css/header-colors.css')}}"/>
+
+	<link href="{{ asset('backend/assets/plugins/datatable/css/dataTables.bootstrap5.min.css')}}" rel="stylesheet" />
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" >
+
+	<title>SISFO SMK | @yield('title') </title>
+</head>
+
+<body>
     <div class="page-content">
         <div class="row justify-content-center">
             <div class="col-md-8">
@@ -32,9 +67,8 @@
             </div>
         </div>
     </div>
-</x-app-layout>
-
-@push('scripts')
+</script>
+@stack('scripts')
 <!-- Load Face API dari CDN -->
 <script src="https://cdn.jsdelivr.net/npm/face-api.js@0.22.2/dist/face-api.min.js"></script>
 <script>
@@ -112,4 +146,6 @@
         });
     });
 </script>
-@endpush
+</body>
+
+</html>
