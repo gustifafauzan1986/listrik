e2510d2b30dbc9a63fc6419f30138c06e2b620db
gustifa <fauzangustifa@gmail.com>
2025-12-03 18:34:24 +0700

    Update Setting

--- resources/views/schedule/index.blade.php
+++ resources/views/schedule/index.blade.php
@@ -114,8 +114,8 @@
             </div>
         </div>
 
-
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    </div>
+        {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script> --}}
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 
@@ -137,5 +137,5 @@
                 })
             }
         </script>
-    </div>
+
 </x-app-layout>
