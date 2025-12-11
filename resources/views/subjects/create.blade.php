ca54ed0acbbae2c95c524720dc30308dd4a0c24c
gustifa74078 <fauzangustifa@gmail.com>
2025-12-11 21:48:37 +0700

    Update Relasi Transkip

--- resources/views/subjects/create.blade.php
+++ resources/views/subjects/create.blade.php
@@ -1,8 +1,7 @@
-
+@section('title', 'Tambah Mata Pelajaran')
 <x-app-layout>
-
-        <div class="container mt-5">
-            <div class="col-md-6 mx-auto">
+    <div class="page-content">
+            <div class="col-md-12 mx-auto">
                 <div class="card shadow">
                     <div class="card-header bg-primary text-white">Tambah Mata Pelajaran</div>
                     <div class="card-body">
@@ -13,15 +12,16 @@
                                 <input type="text" name="name" class="form-control" placeholder="Contoh: Bahasa Indonesia" required>
                             </div>
                             <div class="d-flex justify-content-between">
-                                <a href="{{ route('subjects.index') }}" class="btn btn-secondary">Batal</a>
-                                <button class="btn btn-primary">Simpan</button>
+                                <a href="{{ route('subjects.index') }}" class="btn btn-secondary">
+                                     <i class="fas fa-arrow-left me-1"></i> Batal
+                                </a>
+                                <button class="btn btn-primary">
+                                    <i class="fas fa-save me-1"></i> Simpan
+                                </button>
                             </div>
                         </form>
                     </div>
                 </div>
-            </div>
         </div>
-
-
-        
+    </div>
 </x-app-layout>
