956978bbf85ee06ef4085ca2c913cbd0a391578b
gustifa74078 <fauzangustifa@gmail.com>
2026-03-05 12:00:16 +0700

    Update Card

--- resources/views/print/card_signature.blade.php
+++ resources/views/print/card_signature.blade.php
@@ -3,7 +3,7 @@
                             <div class="sig-title">{{$settings['signature_title'] ?? 'Kepala Sekolah'}}</div>
                             <br>
 
-                            {{-- <img src="{{ asset('images/ttd_kepsek.png') }}" class="sig-img" alt="TTD" onerror="this.style.opacity='0.2'"> --}}
+                            {{-- <img src="{{asset('storage/'.$settings['sign_image'])}}" class="sig-img" alt="TTD" onerror="this.style.opacity='0.2'"> --}}
 
                             <div class="sig-name">{{ $settings['signature_name'] ?? 'Nama Kepala Sekolah' }}</div>
                             <div class="sig-nip">NIP. {{ $settings['signature_nip'] ?? '-' }}</div>
